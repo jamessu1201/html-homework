711dd7fe83c5e1037666786626ba9137b7985b60
james_su <joan3825ms55@gmail.com>
2021-09-28 13:04:11 +0800

    first commit

---
+++ ex.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="zh-Hant-TW">
+<head>
+    <title>This is a test.</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/ex.css" type="text/css">
+    <style>
+        a{
+        text-decoration:none;
+         }
+        a:hover{
+            background-color:white;
+            color:#0081BA !important;
+        }
+    </style>
+</head>
+<body style="margin:0px 0px 0px 0px">
+    <div id="the_top" style="background-color:#0081BA">
+        <div style="font-size:14px">&nbsp;</div>
+        <div style="float:right;padding-right:233px">
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" style="font-size:16px;color:white;padding:6px;border:1px white solid;border-radius:5px;margin-right:5px">新生服務網首頁</a>
+        <a href="https://www.youtube.com/watch?v=dvgZkm1xWPE" target="_blank" style="font-size:16px;color:white;padding:6px;border:1px white solid;border-radius:5px;">中正大學首頁</a>
+        </div>
+    </div>
+    <div class="flex">
+        <div id="left_side" style="margin-left:188px">left side</div>
+        <div id="right_side" style="margin-left:16px;margin-top:16px">
+            <div id="one">
+                <img src="img\banner01.jpg" width="839px" height="306px">
+            </div>
+            <div id="two">最新消息</div>
+            <div id="three">活動消息</div>
+        </div>
+    </div>
+    <div class="flex2" id="the_bottom" style="background-color:#0081BA">
+         <div style="padding-left:230px;padding-top:17px">
+             <img src="img\qrcode.png" width="120px" height="120px">
+         </div>
+         <div style="font-size:20px;color:white;text-align:right;padding-left:520px;line-height:30px">
+         <p>© 2013-2021 版權為國立中正大學所有<br>地址：621301 嘉義縣民雄鄉大學路一段168號<br>總機：05-2720411<br>教務處：11213&nbsp;學務處：12101<br>總務處：13307&nbsp;資訊處：14150</p>
+         </div>
+    </div>
+</body>
+</html>
